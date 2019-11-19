d934bfee6bd375c72932f583d9968071e2b0ac2e
JorgeAntrax <spicer_94@hotmail.com>
2019-11-19 12:57:38 -0700

    First commit v 0.0.5

---
+++ src/app.js
@@ -0,0 +1,44 @@
+'use strict';
+
+window.onload = function() {
+    var $btn = document.querySelector('download-pdf button');
+    $btn.addEventListener('click', function() {
+        var $invoice = this.dataset.invoice;
+        var pdf = new jsPDF('a', 'mm', 'letter');
+        var firstPage = 0;
+
+        let $page = document.querySelector('[page-pdf]');
+        console.log($page);
+
+        html2canvas($page, {
+            onrendered: function(canvas) {
+                firstPage = canvas.toDataURL('image/jpeg', 1.0);
+                pdf.addImage(firstPage, 'JPEG', 0, 0, 216, 0, );
+
+                var blob = pdf.output('blob');
+
+                var data = new FormData();
+                data.append('pdf', blob);
+
+                fetch('url', {
+                        method: 'POST',
+                        body: data
+                    })
+                    .then($r => $r.json())
+                    .catch($r => console.log($r))
+                    .then($r => {
+                        if ($r.status == 'OK') {
+                            pdf.save($r.name);
+                        }
+                    });
+            }
+        });
+    });
+};
+
+// <?php
+// move_uploaded_file(
+//     $_FILES['pdf']['tmp_name'], 
+//     $_SERVER['DOCUMENT_ROOT'] . "/uploads/test.pdf"
+// );
+// ?>
